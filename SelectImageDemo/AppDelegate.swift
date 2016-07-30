a24df8ce3f380ff2937895e25969f5367bd5a1e3
fancymax <lindahai_max@126.com>
2016-07-30 12:51:26 +0800

    add pic

--- SelectImageDemo/AppDelegate.swift
+++ SelectImageDemo/AppDelegate.swift
@@ -15,6 +15,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var imageView: RandCodeImageView2!
     @IBOutlet weak var randCodeLabel: NSTextField!
     
+    var currentImage = "a1.jpeg"
+    var currentCount = 0
+    
+    let picNameArr = ["a1","a2","a3","a4","b1","b2","b3","b4","b5","b6","b7","b8","b9"]
+    
     @IBAction func getRandCode(sender: AnyObject) {
         if imageView.randCodeStr != nil {
             randCodeLabel.stringValue = imageView.randCodeStr!
@@ -24,9 +29,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
 
+    @IBAction func getNextImage(sender: NSButton) {
+        currentCount += 1;
+        if currentCount >= picNameArr.count {
+            currentCount = 0
+        }
+        currentImage = picNameArr[currentCount] + ".jpeg"
+        self.imageView.image = NSImage(named: currentImage)
+        self.imageView.clearRandCodes()
+    }
+    
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
-        let image = NSImage(named: "getPassCodeNew.jpeg")
+        let image = NSImage(named: currentImage)
         self.imageView.image = image
     }
 
